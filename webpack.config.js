556557e1deec28dd1338f966c953060ed23d375c
TakagiTakuro <bgdaewalkman@gmail.com>
2018-10-18 12:26:01 +0900

    webpack hello

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+const path    = require('path');
+
+module.exports = {
+    entry: {
+        bundle: './src/app.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
