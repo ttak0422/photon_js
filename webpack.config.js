bf9b9171c8f5be886445a3d0ec9b6576401abd41
TakagiTakuro <bgdaewalkman@gmail.com>
2018-10-18 12:52:47 +0900

    ts環境を用意

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,25 @@ const path    = require('path');
 
 module.exports = {
     entry: {
-        bundle: './src/app.js'
+        bundle: './src/app.ts'
     },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+        alias: {
+        }
     }
 }
