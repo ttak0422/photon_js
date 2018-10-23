096a7e2cad7cebb621cf6a8d257ab55158f861ab
TakagiTakuro <bgdaewalkman@gmail.com>
2018-10-24 05:42:16 +0900

    [fix] 移動時の描写バグを修正

--- src/visual.ts
+++ src/visual.ts
@@ -17,13 +17,13 @@ export default class Visual {
         this.canvasW = this.canvas.width;
         this.canvasH = this.canvas.height;
         this.context = <CanvasRenderingContext2D>this.canvas.getContext('2d');
-        this.playerW = this.canvasW / 10;
-        this.playerH = this.canvasH / 10;
+        this.playerW = Math.floor(this.canvasW / 10);
+        this.playerH = Math.floor(this.canvasH / 10);
     }
 
     update(x : number, y : number){
-        this.context.clearRect(this.prePosX, this.prePosY, this.prePosX + this.playerW, this.prePosY + this.playerH);
-        this.context.fillRect(x, y, x + this.playerW, y + this.playerH);
+        this.context.clearRect(this.prePosX, this.prePosY, this.playerW, this.playerH);
+        this.context.fillRect(x, y, this.playerW, this.playerH);
         this.prePosX = x;
         this.prePosY = y;
     }
