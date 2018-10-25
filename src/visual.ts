d1f078f6b435c1b3179e3c4790243519187c7bd8
ttak0422 <bgdaewalkman@gmail.com>
2018-10-26 04:10:08 +0900

    fillRectより見た目をマシに

--- src/visual.ts
+++ src/visual.ts
@@ -1,14 +1,14 @@
-import Actor from "./actor";
+import Actor from './actor';
 
 export default class Visual {
     private canvas  : HTMLCanvasElement;
     private context : CanvasRenderingContext2D;
     private canvasW : number;
     private canvasH : number;
-    private prePosX : number = 0;
-    private prePosY : number = 0;
     private playerH : number;
     private playerW : number;
+
+    private player : HTMLDivElement;
 
     constructor(actor : Actor){
         this.canvas  = <HTMLCanvasElement>document.getElementById('canvas');
@@ -17,11 +17,13 @@ export default class Visual {
         this.context = <CanvasRenderingContext2D>this.canvas.getContext('2d');
         this.playerW = Math.floor(this.canvasW / 10);
         this.playerH = Math.floor(this.canvasH / 10);
+        this.player = this.generatePlayerObject();
+        document.body.appendChild(this.player);
         this.update(actor.posX, actor.posY);
     }
 
-    clear(){
-        this.context.clearRect(this.prePosX, this.prePosY, this.playerW, this.playerH);
+    clear() : void {
+        document.body.removeChild(this.player);
     }
 
     /**
@@ -29,11 +31,21 @@ export default class Visual {
      * @param x 移動後の左上の頂点のx座標
      * @param y 移動後の左上の頂点のy座標
      */
-    update(x : number, y : number){
-        this.context.clearRect(this.prePosX, this.prePosY, this.playerW, this.playerH);
-        this.context.fillRect(x, y, this.playerW, this.playerH);
-        this.prePosX = x;
-        this.prePosY = y;
+    update(x : number, y : number) : void {
+        this.player.style.top = `${y}px`;
+        this.player.style.left = `${x}px`;
+    }
+
+    // TODO : 色
+    private generatePlayerObject() : HTMLDivElement {
+        const newElem = document.createElement('div');
+        newElem.className = 'player';
+        newElem.style.backgroundColor = 'yellow';
+        newElem.style.width  = `${this.playerW}px`;
+        newElem.style.height = `${this.playerH}px`;
+        newElem.style.position = 'fixed'
+        newElem.style.zIndex = '1';
+        return newElem;
     }
 
 }
